038e8a261d4ca2590dbf8fc708dde8f614b98cb7
sambrolia <samuel_brolia@msn.com>
2023-10-26 14:54:05 +0100

    doing well

---
+++ exercises/primitive_types/primitive_types1.rs
@@ -0,0 +1,18 @@
+// primitive_types1.rs
+//
+// Fill in the rest of the line that has code missing! No hints, there's no
+// tricks, just get used to typing these :)
+
+fn main() {
+    // Booleans (`bool`)
+
+    let is_morning = true;
+    let is_evening = false;
+    if is_morning {
+        println!("Good morning!");
+    }
+
+    if is_evening {
+        println!("Good evening!");
+    }
+}
